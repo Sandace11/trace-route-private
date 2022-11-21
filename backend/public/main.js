4b2e81ac76a74e01ef60c49f0bda039626fd86e2
Sandace11 <075bct075.sandesh@pcampus.edu.np>
2022-11-21 21:00:47 +0545

    Added lines and points together

--- backend/public/main.js
+++ backend/public/main.js
@@ -2,16 +2,18 @@ window.onload = init;
 
 // try using Leaflet
 let iconFeatures = new Array();
+let lineFeatures = new Array();
 
 const locations = [
-    "27.6766,85.3142",
-    "29.7179,-95.4263",
+    "27.3256,86.0877",
+    "19.0728,72.8826",
+    "37.5483,-121.9886",
     "27.7017,85.3206",
     "27.6766,85.3142",
-    "27.6766,85.3142",
-    "27.6766,85.3142",
-    "27.6766,85.3142",
-    "27.7017,85.3206"
+    "27.7017,85.3206",
+    "37.5483,-121.9886",
+    "53.3331,-6.2489",
+    "53.3331,-6.2489"
 ]
 
 function init() {
@@ -50,6 +52,50 @@ function init() {
         })
     });
 
+    // // Pins in locations
+    // locations.forEach((location) => {
+    //     const [lat, long] = location.split(',');
+    //     iconFeatures.push(new ol.Feature({
+    //         geometry: new ol.geom.Point(ol.proj.fromLonLat([long, lat]))
+    //     }))
+    // })
+
+    // iconFeatures.forEach(function (feature) { feature.setStyle(myStyle) });
+
+    // const vectorLayer = new ol.layer.Vector({
+    //     source: new ol.source.Vector({
+    //         features: iconFeatures
+    //     })
+    // });
+
+    // map.addLayer(vectorLayer);
+
+
+    // // Lines between locations
+    // locations.forEach((location, index) => {
+    //     if (index === locations.length - 1) return;
+
+    //     const [lat1, long1] = location.split(',');
+    //     const [lat2, long2] = locations[index + 1].split(',');
+
+    //     lineFeatures.push(new ol.Feature({
+    //         geometry: new ol.geom.LineString([
+    //             ol.proj.fromLonLat([long1, lat1]),
+    //             ol.proj.fromLonLat([long2, lat2])
+    //         ])
+    //     }))
+    // })
+
+    // lineFeatures.forEach(function (feature) { feature.setStyle(myStyle) });
+
+    // const lineLayer = new ol.layer.Vector({
+    //     source: new ol.source.Vector({
+    //         features: lineFeatures
+    //     })
+    // });
+
+    // map.addLayer(lineLayer);
+
     const form = document.querySelector('form');
 
     form.addEventListener('submit', (e) => {
@@ -67,8 +113,9 @@ function init() {
                 if (data.error) {
                     return console.log(data);
                 } else {
-                    console.log(data.coords);
+                    console.log(data);
 
+                    // Pins in locations
                     data.coords.forEach((location) => {
                         const [lat, long] = location.split(',');
                         iconFeatures.push(new ol.Feature({
@@ -85,8 +132,33 @@ function init() {
                     });
 
                     map.addLayer(vectorLayer);
+
+                    // Lines between locations
+                    data.coords.forEach((location, index) => {
+                        if (!location || index === locations.length - 1) return;
+
+                        const [lat1, long1] = location.split(',');
+                        const [lat2, long2] = locations[index + 1].split(',');
+
+                        lineFeatures.push(new ol.Feature({
+                            geometry: new ol.geom.LineString([
+                                ol.proj.fromLonLat([long1, lat1]),
+                                ol.proj.fromLonLat([long2, lat2])
+                            ])
+                        }))
+                    })
+
+                    lineFeatures.forEach(function (feature) { feature.setStyle(myStyle) });
+
+                    const lineLayer = new ol.layer.Vector({
+                        source: new ol.source.Vector({
+                            features: lineFeatures
+                        })
+                    });
+
+                    map.addLayer(lineLayer);
                 }
             })
-
     })
 }
+
